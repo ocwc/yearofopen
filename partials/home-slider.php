87943c576ee4084de3e4be4753977cffeb0273a4
Jure Cuhalev <jure@cuhalev.com>
2017-01-17 13:41:47 +0100

    Partner size

--- partials/home-slider.php
+++ partials/home-slider.php
@@ -19,7 +19,7 @@
 
                         <div class="small-12 medium-4 columns partner__single">
                             <?php if ( get_field('partner_url') ) : ?>
-                                <a href="<?php the_field('partner_url'); ?>"><?php the_post_thumbnail(); ?></a>
+                                <a href="<?php the_field('partner_url'); ?>"><?php the_post_thumbnail('medium'); ?></a>
                             <?php else : ?>
                                 <?php the_post_thumbnail(); ?>
                             <?php endif; ?>
