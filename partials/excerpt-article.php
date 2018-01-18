b23becb9df58c4feb631206cb12e82f052770567
Jure Cuhalev <jure@cuhalev.com>
2018-01-18 16:25:13 +0100

    Read more link after exceprts

--- partials/excerpt-article.php
+++ partials/excerpt-article.php
@@ -15,7 +15,7 @@
     </div>
     <div class="cell medium-7 small-expanded">
         <h3><a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a></h3>
-        <?php the_excerpt(); ?>
+        <?php the_excerpt(); ?><a href="<?php the_permalink(); ?>" >Read more ..</a>
     </div>
 
     <div class="cell medium-offset-1 medium-10 article-divider"></div>
